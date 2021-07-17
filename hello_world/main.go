59530b721154b6d7259c575bab75f8a499bad0d7
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-07-17 20:37:44 +0600

    initial commit

---
+++ hello_world/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello World")
+	// x := 1
+	// fmt.Println(x)
+	// x = x + 2
+	// fmt.Println(x)
+	var x int
+	fmt.Println(x)
+
+	var y bool
+	fmt.Println(y)
+
+	var z float32
+	fmt.Println(z)
+
+	var a string
+	fmt.Println(a)
+}
---
+++ variable/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string
+	// this is an example of variable declaration.
+	// It creats a new variable  It creates a new variable with a name (x) and a type (string).
+
+	x = "This is Golang"
+
+	//  We then assign a value to that variable (Hello World)
+	fmt.Println(x)
+
+	//  and then retrieve the value when we send it to fmt.Println.
+}
