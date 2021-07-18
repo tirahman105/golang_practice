511773562cfb0110cb7b3caf218a45f23247be27
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-07-18 07:20:59 +0600

    arithmatic operations

---
+++ 4_arith_demo/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	//declare variables
+
+	var a, b int
+
+	// assign value
+
+	a = 5
+	b = 10
+
+	//arithmetic operation
+	//addition
+
+	c := a + b
+	fmt.Printf("%d + %d = %d  \n", a, b, c)
+
+	//subtraction
+
+	d := a - b
+	fmt.Printf("%d - %d = %d \n", a, b, d)
+
+	//division
+
+	e := float32(a) / float32(b)
+	fmt.Printf("%d / %d = %.2f \n", a, b, e)
+
+	//multiplication
+
+	f := a * b
+	fmt.Printf("%d * %d = %d", a, b, f)
+}
